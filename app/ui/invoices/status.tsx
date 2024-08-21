ede10aae330dfca4ad9f42a9d3b54e735654c744
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 21:34:20 +0330

    table styles

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,16 +1,17 @@
+import { Badge } from '@/components/ui/badge';
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
-    <span
+    <Badge
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
+          'bg-green-600': status === 'paid',
         },
       )}
+      variant='secondary'
     >
       {status === 'pending' ? (
         <>
@@ -24,6 +25,6 @@ export default function InvoiceStatus({ status }: { status: string }) {
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
-    </span>
+    </Badge>
   );
 }
