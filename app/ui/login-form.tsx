3172c76c7d45ef30a9c4f04ae61743c805efca1b
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 15:12:16 +0330

    login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,6 +8,7 @@ import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "@/components/ui/button";
 import { useActionState } from "react";
 import { authenticate } from "@/app/lib/actions";
+import { Input } from "@/components/ui/input";
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
@@ -17,53 +18,53 @@ export default function LoginForm() {
 
   return (
     <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+      <div className="flex-1 rounded-lg border-2 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl`}>Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-xs font-medium"
               htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+              <Input
+                className="peer block w-full rounded-md border py-[9px] pl-10 text-sm outline-2"
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
+                defaultValue={"user@nextmail.com"}
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
             </div>
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-xs font-medium"
               htmlFor="password"
             >
               Password
             </label>
             <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+              <Input
+                className="peer block w-full rounded-md border py-[9px] pl-10 text-sm outline-2 "
                 id="password"
                 type="password"
                 name="password"
                 placeholder="Enter password"
                 required
                 minLength={6}
+                defaultValue={"123456"}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2" />
             </div>
           </div>
         </div>
         <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          Log in <ArrowRightIcon className="ml-auto h-5 w-5" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
