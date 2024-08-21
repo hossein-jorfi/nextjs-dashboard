858086c8661487bde6f9f08fdb52b8cb5310952d
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 14:34:57 +0330

    add button component

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,25 @@ import { ArrowRightIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 // assets
 import AcmeLogo from "@/app/ui/acme-logo";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg border-2 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-900 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`text-xl md:text-3xl md:leading-normal`}>
+            <strong>Welcome,</strong> This is the example for the{" "}
+            <a href="https://nextjs.org/learn/" className="text-blue-600">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
           </p>
           <Link href="/login" className="">
             <Button className="flex gap-1">
               Login
-              {/* <ArrowRightIcon className="w-5" /> */}
               <ChevronRightIcon className="h-4 w-4" />
             </Button>
           </Link>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -5,7 +5,7 @@ import {
   ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
-import { Button } from "./button";
+import { Button } from "@/components/ui/button";
 import { useActionState } from "react";
 import { authenticate } from "@/app/lib/actions";
 
