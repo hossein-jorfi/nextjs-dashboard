27e37012d18dc90216a7714d934e442eea758b87
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 15:50:04 +0330

    side nav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -33,9 +33,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 border-2 border-transparent hover:border-border",
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 border-2 border-transparent hover:border-border text-gray-600",
               {
-                "border-primary hover:border-primary": pathname === link.href,
+                "text-primary": pathname === link.href,
               }
             )}
           >
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -24,7 +24,7 @@ export default function SideNav() {
             await signOut();
           }}
         >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 border-2 border-transparent hover:border-primary">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 border-2 border-transparent hover:border-destructive hover:text-destructive hover:text-red-500">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
