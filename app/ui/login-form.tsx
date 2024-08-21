2b221f5b3d9ec9803d3bcdfa0f779311a9cef23f
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 15:24:44 +0330

    login form loading

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -3,12 +3,13 @@ import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/24/outline";
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "@/components/ui/button";
 import { useActionState } from "react";
 import { authenticate } from "@/app/lib/actions";
 import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
 
 export default function LoginForm() {
   const [errorMessage, formAction, isPending] = useActionState(
@@ -63,8 +64,18 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5" />
+        <Button
+          className="mt-4 w-full"
+          aria-disabled={isPending}
+          disabled={isPending}
+        >
+          {isPending ? (
+            <Loader2 className="mr-2 h-6 w-6 animate-spin" />
+          ) : (
+            <>
+              Log in <ChevronRightIcon className="ml-auto h-4 w-4" />
+            </>
+          )}
         </Button>
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
