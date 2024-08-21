7314fd62196f60a173ae41c4ee26a89093564c60
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 15:41:50 +0330

    side nav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -34,7 +34,9 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3 border-2 border-transparent hover:border-border",
-              { "border-muted": pathname === link.href }
+              {
+                "border-primary hover:border-primary": pathname === link.href,
+              }
             )}
           >
             <LinkIcon className="w-6" />
