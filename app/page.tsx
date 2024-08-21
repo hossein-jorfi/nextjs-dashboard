a02f383c43ec05314eb2ecbe4bf65a6067b7346c
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 14:28:47 +0330

    add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 // components
 import Link from "next/link";
 import Image from "next/image";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 // assets
 import AcmeLogo from "@/app/ui/acme-logo";
+import { Button } from "./ui/button";
 
 export default function Page() {
   return (
@@ -21,11 +22,12 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          <Link href="/login" className="">
+            <Button className="flex gap-1">
+              Login
+              {/* <ArrowRightIcon className="w-5" /> */}
+              <ChevronRightIcon className="h-4 w-4" />
+            </Button>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
