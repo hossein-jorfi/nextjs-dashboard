791bf46841327fc82135d810b51843dcf4232dcc
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 14:53:56 +0330

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // components
 import Link from "next/link";
 import Image from "next/image";
-import { ArrowRightIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 // assets
 import AcmeLogo from "@/app/ui/acme-logo";
@@ -28,21 +28,10 @@ export default function Page() {
             </Button>
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="md:hidden block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
+        <div className="flex items-center justify-center md:w-3/5 border-2 rounded-lg p-10">
+          <div className="w-60 md:w-80 h-60 md:h-80 rounded-full border-2 border-dotted flex justify-center items-center text-border">
+            Image
+          </div>
         </div>
       </div>
     </main>
