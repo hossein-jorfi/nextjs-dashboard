3243b011e4eb8c54a82325bb87a63e8f592b9dd7
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-21 17:45:19 +0330

    LatestInvoices styles

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -26,12 +26,13 @@ export default async function Page() {
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
+        {/* <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-        </Suspense>
-        {/* <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
         </Suspense> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        <LatestInvoicesSkeleton />
       </div>
     </main>
   );
