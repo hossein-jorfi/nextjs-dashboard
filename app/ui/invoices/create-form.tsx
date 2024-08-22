77093eaaa77fabcc1816629065e1ab7b8e5b7a7e
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-22 14:43:55 +0330

    amount Input

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -18,6 +18,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
 
 const initialState: State = { message: null, errors: {} };
 export default function Form({ customers }: { customers: CustomerField[] }) {
@@ -25,7 +26,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
   return (
     <form action={formAction}>
-      <div className="rounded-md border-2 p-4 md:p-6">
+      <div className="rounded-lg border-2 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -33,7 +34,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </label>
 
           <Select>
-            <SelectTrigger className="w-full">
+            <SelectTrigger className="w-full pl-10 relative text-muted-foreground checked:text-red-500">
+              <UserCircleIcon className="w-[18px] h-[18px] absolute left-3" />
               <SelectValue id="customer" placeholder="Customer" />
             </SelectTrigger>
             <SelectContent className="">
@@ -53,15 +55,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
-              <input
+              <Input
                 id="amount"
                 name="amount"
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="block w-full border py-2 pl-10 text-sm text-muted-foreground"
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-muted-foreground peer-focus:text-primary " />
             </div>
           </div>
           {state.errors?.amount &&
