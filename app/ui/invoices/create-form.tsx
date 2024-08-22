7bcfbed5f315babca7cf86c6fe6ab1b06eba82bd
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-22 14:25:22 +0330

    select component

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,6 +11,13 @@ import {
 import { Button } from "@/components/ui/button";
 import { createInvoice, State } from "@/app/lib/actions";
 import { useActionState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const initialState: State = { message: null, errors: {} };
 export default function Form({ customers }: { customers: CustomerField[] }) {
@@ -18,39 +25,25 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
   return (
     <form action={formAction}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="rounded-md border-2 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
           </label>
-          <div className="relative">
-            <select
-              id="customer"
-              name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
-              aria-describedby="customer-error"
-            >
-              <option value="" disabled>
-                Select a customer
-              </option>
+
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue id="customer" placeholder="Customer" />
+            </SelectTrigger>
+            <SelectContent className="">
               {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
+                <SelectItem key={customer.id} value={customer.id}>
                   {customer.name}
-                </option>
+                </SelectItem>
               ))}
-            </select>
-            <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-          </div>
-          <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.customerId &&
-              state.errors.customerId.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>
+            </SelectContent>
+          </Select>
         </div>
 
         {/* Invoice Amount */}
@@ -72,11 +65,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
           {state.errors?.amount &&
-              state.errors?.amount.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            state.errors?.amount.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
         </div>
 
         {/* Invoice Status */}
@@ -119,11 +112,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
           {state.errors?.amount &&
-              state.errors?.amount.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
+            state.errors?.amount.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
