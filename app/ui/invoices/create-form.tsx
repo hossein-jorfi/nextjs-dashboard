05fef4f6fdb7af6f69cd82dbd1a9a434dde513a1
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-22 15:14:03 +0330

    form Radio Group

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -19,6 +19,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const initialState: State = { message: null, errors: {} };
 export default function Form({ customers }: { customers: CustomerField[] }) {
@@ -79,42 +81,24 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
           </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
-                <input
-                  id="pending"
-                  name="status"
-                  type="radio"
-                  value="pending"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                />
-                <label
-                  htmlFor="pending"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
-                >
-                  Pending <ClockIcon className="h-4 w-4" />
-                </label>
+          <div className="rounded-md px-[14px] border py-3">
+            <RadioGroup
+              defaultValue="comfortable"
+              className="flex gap-5"
+              name="status"
+            >
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem id="pending" value="pending" />
+                <Label htmlFor="pending">Pending</Label>
               </div>
-              <div className="flex items-center">
-                <input
-                  id="paid"
-                  name="status"
-                  type="radio"
-                  value="paid"
-                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                />
-                <label
-                  htmlFor="paid"
-                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
-                >
-                  Paid <CheckIcon className="h-4 w-4" />
-                </label>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem id="paid" value="paid" />
+                <Label htmlFor="paid">Paid</Label>
               </div>
-            </div>
+            </RadioGroup>
           </div>
-          {state.errors?.amount &&
-            state.errors?.amount.map((error: string) => (
+          {state.errors?.status &&
+            state.errors?.status.map((error: string) => (
               <p className="mt-2 text-sm text-red-500" key={error}>
                 {error}
               </p>
