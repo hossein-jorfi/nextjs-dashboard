59e21ca9a2025f4b19b8d282b4865e1a0914f712
hosseinjorfi <hosseinjorfi81@gmail.com>
2024-08-24 08:45:21 +0330

    styles

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
-import Search from '@/app/ui/search';
+import Image from "next/image";
+import Search from "@/app/ui/search";
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '@/app/lib/definitions';
+} from "@/app/lib/definitions";
 
 export default async function CustomersTable({
   customers,
@@ -12,20 +12,15 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={`mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
+      <h1 className={`mb-8 text-xl md:text-2xl`}>Customers</h1>
       <Search placeholder="Search customers..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
-            <div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
+            <div className="overflow-hidden rounded-md border-2 p-2 md:pt-0">
               <div className="md:hidden">
                 {customers?.map((customer) => (
-                  <div
-                    key={customer.id}
-                    className="mb-2 w-full rounded-md bg-white p-4"
-                  >
+                  <div key={customer.id} className="mb-2 w-full rounded-md p-4">
                     <div className="flex items-center justify-between border-b pb-4">
                       <div>
                         <div className="mb-2 flex items-center">
@@ -40,9 +35,7 @@ export default async function CustomersTable({
                             <p>{customer.name}</p>
                           </div>
                         </div>
-                        <p className="text-sm text-gray-500">
-                          {customer.email}
-                        </p>
+                        <p className="text-sm">{customer.email}</p>
                       </div>
                     </div>
                     <div className="flex w-full items-center justify-between border-b py-5">
@@ -61,8 +54,8 @@ export default async function CustomersTable({
                   </div>
                 ))}
               </div>
-              <table className="hidden min-w-full rounded-md text-gray-900 md:table">
-                <thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
+              <table className="hidden min-w-full rounded-md md:table">
+                <thead className="rounded-md text-left text-sm font-normal">
                   <tr>
                     <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
                       Name
@@ -82,10 +75,13 @@ export default async function CustomersTable({
                   </tr>
                 </thead>
 
-                <tbody className="divide-y divide-gray-200 text-gray-900">
+                <tbody className="divide-y first:!rounded-lg">
                   {customers?.map((customer) => (
-                    <tr key={customer.id} className="group">
-                      <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
+                    <tr
+                      key={customer.id}
+                      className="group"
+                    >
+                      <td className="whitespace-nowrappy-5 pl-4 pr-3 text-sm sm:pl-6 bg-primary-foreground">
                         <div className="flex items-center gap-3">
                           <Image
                             src={customer.image_url}
@@ -97,16 +93,16 @@ export default async function CustomersTable({
                           <p>{customer.name}</p>
                         </div>
                       </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      <td className="whitespace-nowrap px-4 py-5 text-sm bg-primary-foreground">
                         {customer.email}
                       </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      <td className="whitespace-nowrap px-4 py-5 text-sm bg-primary-foreground">
                         {customer.total_invoices}
                       </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+                      <td className="whitespace-nowrap px-4 py-5 text-sm bg-primary-foreground">
                         {customer.total_pending}
                       </td>
-                      <td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
+                      <td className="whitespace-nowrap px-4 py-5 text-sm bg-primary-foreground">
                         {customer.total_paid}
                       </td>
                     </tr>
